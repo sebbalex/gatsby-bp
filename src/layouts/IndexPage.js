fa7286bb06908ad582efc25649535e806bed339a
Alessandro Sebastiani <sebbalex@gmail.com>
2021-10-07 23:55:36 +0200

    feat: new form

--- src/layouts/IndexPage.js
+++ src/layouts/IndexPage.js
@@ -3,12 +3,14 @@ import content from '../../contents/home-page/home.yml';
 import { NewsPreviewSection } from './home/NewsPreviewSection.js';
 import { StrategySection } from './home/StrategySection.js';
 import { BenefitSection } from './home/BenefitSection.js';
+import { Form } from './home/Form';
 
 export const IndexPage = () => (
   <>
     <div className="sr-only">
       <h1>{content.name}</h1>
     </div>
+    <Form />
     <StrategySection />
     <BenefitSection />
     <NewsPreviewSection />
